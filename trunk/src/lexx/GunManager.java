adb698102148c39797e22ee179583b88b61cc42f
stefan.untereichner <stefan.untereichner@dbdab378-8051-0410-977f-5940ee3aa3a4>
2008-07-09 06:40:26 +0000

    o adding dummy GunManager implementation + build file

---
+++ trunk/src/lexx/GunManager.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package lexx;
+
+import java.awt.Graphics2D;
+
+public class GunManager {
+
+	/**
+	 * @param dasBot
+	 * @param currentTarget
+	 */
+	public GunManager(DasBot dasBot, Target currentTarget) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @return
+	 */
+	public VirtualGun getBestGun() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * 
+	 */
+	public void trackFire() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param g
+	 */
+	public void onPaint(Graphics2D g) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
