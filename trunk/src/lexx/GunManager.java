972e4082e8f6cc6f0778ab97a0c5935012de14b3
alexander.floh <alexander.floh@dbdab378-8051-0410-977f-5940ee3aa3a4>
2008-07-09 17:50:42 +0000

    added gunmanager implementation

--- trunk/src/lexx/GunManager.java
+++ trunk/src/lexx/GunManager.java
@@ -1,50 +1,68 @@
-/**
- * 
- */
 package lexx;
 
 import java.awt.Graphics2D;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 
 public class GunManager {
+  private List<VirtualGun> guns;
+  private Target target;
+  private DasBot robot;
+  
+  private Comparator<VirtualGun> gunComparator = new SimpleGunComparator();
+  
+  public GunManager(DasBot robot, Target target) {
+    this.target = target;
+    this.robot = robot;
+    guns = new LinkedList<VirtualGun>();
+    guns.add(new DirectGun(robot, target));
+    guns.add(new LinearGun(robot, target, -1.0));
+    guns.add(new LinearGun(robot, target, -0.8));
+    guns.add(new LinearGun(robot, target, -0.6));
+    guns.add(new LinearGun(robot, target, -0.4));
+    guns.add(new LinearGun(robot, target, -0.2));
+    guns.add(new LinearGun(robot, target, 0.2));
+    guns.add(new LinearGun(robot, target, 0.4));
+    guns.add(new LinearGun(robot, target, 0.6));
+    guns.add(new LinearGun(robot, target, 0.8));
+    guns.add(new LinearGun(robot, target, 1));
+  }
+  
+  public void update() {
+    for (VirtualGun gun : guns) {
+      gun.update();
+    }
+  }
+  
+  public void trackFire() {
+    for (VirtualGun gun : guns) {
+      gun.doFire();
+    }
+  }
 
-	/**
-	 * @param dasBot
-	 * @param currentTarget
-	 */
-	public GunManager(DasBot dasBot, Target currentTarget) {
-		// TODO Auto-generated constructor stub
-	}
+  public VirtualGun getBestGun() {
+    Collections.sort(guns, gunComparator);
+    robot.out.println(guns);
+    VirtualGun bestGun = guns.get(guns.size() - 1);
+    return bestGun;
+  }
+  
+  
+  private static class SimpleGunComparator implements Comparator<VirtualGun> {
 
-	/**
-	 * 
-	 */
-	public void update() {
-		// TODO Auto-generated method stub
+    @Override
+    public int compare(VirtualGun o1, VirtualGun o2) {
+      return o1.getBulletsHit() - o2.getBulletsHit();
+    }
+    
+  }
 
-	}
 
-	/**
-	 * @return
-	 */
-	public VirtualGun getBestGun() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/**
-	 * 
-	 */
-	public void trackFire() {
-		// TODO Auto-generated method stub
-
-	}
-
-	/**
-	 * @param g
-	 */
-	public void onPaint(Graphics2D g) {
-		// TODO Auto-generated method stub
-
-	}
-
+  public void onPaint(Graphics2D g) {
+    for (VirtualGun gun : guns) {
+      gun.onPaint(g);
+    }
+  }
 }
