886fca13ed35f3970a370aeeae6cef9850cd798e
stefan.untereichner <stefan.untereichner@dbdab378-8051-0410-977f-5940ee3aa3a4>
2008-07-09 13:36:11 +0000

    + algorithm to move to a point

--- src/us/Target.java
+++ src/us/Target.java
@@ -2,11 +2,18 @@ package us;
 
 import robocode.ScannedRobotEvent;
 
+/**
+ * @author Stefan Untereichner
+ * 
+ */
 public class Target extends ScannedRobotEvent {
 
+	/**
+	 * @param event
+	 */
 	public Target(ScannedRobotEvent event) {
-		super(event.getName(), event.getEnergy(), event.getBearingRadians(), event.getDistance(),
-				event.getHeadingRadians(), event.getVelocity());
+		super(event.getName(), event.getEnergy(), event.getBearingRadians(), event.getDistance(), event
+				.getHeadingRadians(), event.getVelocity());
 	}
 
 	@Override
